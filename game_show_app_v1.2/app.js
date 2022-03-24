69a11b3df99f4e827b9a77bc272ba2f5a3131672
spikefreeman <97442624+spikefreeman@users.noreply.github.com>
2022-03-24 10:29:36 +0000

    First final build

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -4,18 +4,19 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startBtn = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
+const title = document.querySelector('.title')
 
 // Number of wrong guesses
 let missed = 0;
 
 //Array of phrases
-const phrases = ["As Good As It Gets", 
-                "Tough Love", 
-                "Long Live The Queen", 
-                "Any Port In A Storm", 
-                "Saved By The Bell"];
-
-
+const phrases = [
+    "As Good As It Gets", 
+    "Tough Love", 
+    "Long Live The Queen", 
+    "Any Port In A Storm", 
+    "Saved By The Bell"
+];
 
 
 
@@ -32,17 +33,17 @@ function getRandomPhraseAsArray(arr){
     const randomNumber = Math.floor(Math.random() * arr.length);
     //Select an index inside the array
     const randomPhrase = arr[randomNumber];
-    //Return statement
+    //Return statement splitting
     return randomPhrase.split('');
 };
 
-//Call get random phrase 
-const phraseArray = getRandomPhraseAsArray(phrases);
 
-
-
+//Get random phrase and store it
+let phraseArray = getRandomPhraseAsArray(phrases);
 //Call add phrase to display
 addPhraseToDisplay(phraseArray);
+
+
 
 //Puts phrase up on the board
 function addPhraseToDisplay(arr){
@@ -66,17 +67,20 @@ function addPhraseToDisplay(arr){
 }
 
 
+
+
+
 //Check the selected letter against the answer 
 function checkLetter (button){
     //Store correct letters 
-    const correct = document.querySelectorAll('li');
+    const checkLetter = document.querySelectorAll('li');
     let match = null;
 
     //Loop throught to check if correct
     for (i = 0; i < checkLetter.length; i++){
         //if clicked button is equal to a letter in the phrase index (to lowercase)
-        if (button.textContent === correct[i].textContent.toLowerCase()){
-            correct[i].classList.add('show');
+        if (button.textContent === checkLetter[i].textContent.toLowerCase() ) {
+            checkLetter[i].classList.add('show');
             match = button.textContent;
         } 
     }
@@ -85,18 +89,48 @@ function checkLetter (button){
     return match;
 }
 
-checkLetter(addPhraseToDisplay);
+//checkLetter(addPhraseToDisplay);
+const checkWin = () => {
+    const letters = document.querySelectorAll('.letter');
+    const show = document.querySelectorAll('.show');
+
+    if(letters.length === show.length){
+        overlay.className = 'win';
+        title.textContent = "You Win!";
+        overlay.style.display = 'flex';
+    }
+
+    if (missed > 4) {
+        overlay.className = 'lose';
+        title.textContent = `You lost! Please try again!'`;
+        overlay.style.display = 'flex';
+        startButton.textContent = 'Try Again!';
+        startButton.addEventListener('click', () => { 
+            setPlayAgain();
+        });
+    }
+};
 
 
-//Listen for selecting a letter 
+///Listen for button click on screen keyboard
 qwerty.addEventListener('click', (e) => {
-
-    //Place clicked variable
-    let clicked = e.target;
-
-    //Check if clicked button and if not chosen
-    if (clicked.tagName === "BUTTON" || clicked.className != "chosen"){
-        clicked.className = 'chosen';
-        let letterFound = checkLetter(clicked);
-    } 
+        
+    let buttonClick = e.target;
+    // Check if button has chosen class
+    if (buttonClick.tagName === 'BUTTON' || buttonClick.className === 'chosen') {
+        buttonClick.className = 'chosen'; // Add the chosen class to the button that was pressed.
+        buttonClick.disabled =  true; // Disabled button once clicked
+        const letterFound = checkLetter(buttonClick);
+        
+        // Check if function find a letter, and remove heart and increment missed counter
+        if ( letterFound === null ) {
+            const lost = document.querySelectorAll('.tries img');
+            lost[missed].src = 'images/lostHeart.png';
+            lost.className = 'lost';
+            missed++;
+        }
+    }
+    checkWin();
 });
+
+
